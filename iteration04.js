265824c2acfb2bf6f80865102aeecec2728483c3
Juan Rodriguez Talavera <juanrodrigueztalavera@gmail.com>
2023-01-08 20:05:19 +0000

    es6

---
+++ iteration04.js
@@ -0,0 +1,68 @@
+/* 4.1 Dado el siguiente array, devuelve un array con sus nombres 
+utilizando .map().
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+]; */
+
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const copyUsers = users.map(user => user.name)
+console.log(copyUsers) 
+
+/* 4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
+empiece por 'A'.
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+]; */
+
+const usersNew = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+console.log('En el ejercicio 4.2, el nombre de los users es: ', usersNew)
+
+const copyUsersNew = users.map((user) => {
+    return user.name.charAt(0) === 'A' ? user.name = 'Anacleto' : user.name;
+})
+console.log('En el ejercicio 4.2, el nombre de los users nuevos tras el cambio es: ', copyUsersNew) 
+
+/* 4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
+cuando el valor de la propiedad isVisited = true.
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+]; */
+
+
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
+
+const visitedCities = cities.map((city) =>{
+    if (city.isVisited === true){
+        city.name += ": Visitado";
+    }    return city.name
+}
+)
+console.log('Estas son las ciudades agregando si han sido visitadas', visitedCities)
